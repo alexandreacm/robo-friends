e4b3567ecd3fc19f7141680ae4d4185b83d7bece
Alexandre Marques <alexandreacm.marques@gmail.com>
2023-10-19 18:32:25 -0300

    first commit.

---
+++ src/CardList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from '../src/Card';
+
+const CardList = ({ data }) => {
+    return (
+        <div>
+
+            {data.map((user, idx) => {
+                return <Card
+                    key={user.id}
+                    id={user.id}
+                    name={user.name}
+                    email={user.email} />
+            })}
+        </div>
+    )
+}
+
+export default CardList;
