e006a107a7034fbd404e8e21772c3cba1f2cf52e
Alexandre Marques <alexandreacm.marques@gmail.com>
2023-10-20 09:01:16 -0300

    Removed mock and added fetch with the url.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots'
+// import { robots } from './robots'
 import './App.css'
 class App extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: '',
         }
     }
 
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
+    }
+
+
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value });    
     }
-
+    
     render() {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase());
         });  
 
-        console.log('Rendered App..');
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList data={filteredRobots} />
-            </div>
-        )
+        if (this.state.robots.length === 0) {
+            return (
+                <p className='loading'>Loading...</p>
+            )
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList data={filteredRobots} />
+                </div>
+            )
+        }
+       
     }
 
 }
