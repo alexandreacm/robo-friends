eb2663e2215c48e5e5e8177c01c05c3bb88e93cd
Alexandre Marques <alexandreacm.marques@gmail.com>
2024-02-29 15:41:01 -0300

    Some refactorings.

--- src/containers/App.js
+++ src/containers/App.js
@@ -23,19 +23,19 @@ class App extends Component {
 
 
     onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value });    
+        this.setState({ searchfield: event.target.value });
     }
-    
+
     render() {
         const { robots, searchfield } = this.state;
 
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase());
-        });  
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        });
 
         return !robots.length ?
             <p className='tc loading'>Loading...</p> :
-            
+
             (<div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
@@ -44,7 +44,7 @@ class App extends Component {
                 </Scroll>
             </div>
             );
-       
+
     }
 
 }
